019bf64eb6f7f20dd2d8dfb02c722064bf0807a4
Owen Orlando <oorlando@student.ciputra.ac.id>
2024-04-25 12:39:59 +0700

    Initial Commit

---
+++ Landmarks/Views/Helper/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by student on 18/04/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+
+
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+
+#Preview {
+    CircleImage(image: Image("turtlerock"))
+}
