30cadd1fe087cd6591edd4bd8a021d195a27512f
Owen Orlando <oorlando@student.ciputra.ac.id>
2024-04-26 08:48:33 +0700

    Sub 2 Chapter 4

--- WatchLandmark Watch App/ContentView.swift
+++ WatchLandmark Watch App/ContentView.swift
@@ -6,10 +6,18 @@
 //
 
 import SwiftUI
+import UserNotifications
+
 
 struct ContentView: View {
     var body: some View {
         LandmarkList()
+            .task {
+                let center = UNUserNotificationCenter.current()
+                _ = try? await center.requestAuthorization(
+                    options: [.alert, .sound, .badge]
+                )
+            }
     }
 }
 
--- WatchLandmark Watch App/NotificationController.swift
+++ WatchLandmark Watch App/NotificationController.swift
@@ -5,4 +5,46 @@
 //  Created by student on 26/04/24.
 //
 
-import Foundation
+import WatchKit
+import SwiftUI
+import UserNotifications
+
+
+class NotificationController: WKUserNotificationHostingController<NotificationView> {
+    var landmark: Landmark?
+    var title: String?
+    var message: String?
+    
+    let landmarkIndexKey = "landmarkIndex"
+    
+    override var body: NotificationView {
+        NotificationView(
+            title: title,
+            message: message,
+            landmark: landmark
+        )
+    }
+
+
+    override func didReceive(_ notification: UNNotification) {
+        let modelData = ModelData()
+
+
+        let notificationData =
+            notification.request.content.userInfo as? [String: Any]
+
+
+        let aps = notificationData?["aps"] as? [String: Any]
+        let alert = aps?["alert"] as? [String: Any]
+
+
+        title = alert?["title"] as? String
+        message = alert?["body"] as? String
+
+
+        if let index = notificationData?[landmarkIndexKey] as? Int {
+            landmark = modelData.landmarks[index]
+        }
+    }
+}
+
